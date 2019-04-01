8e86bde089a58801220ca161437b98d51e986d0f
jyothsnadeshpande <jyothsnajdeshpande@gmail.com>
2019-04-01 11:22:32 +0530

    Update script_1.js

--- script_1.js
+++ script_1.js
@@ -1,40 +1,62 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* global getAssetRegistry getParticipantRegistry */
+
+/**
+ * Close the bidding for a vehicle listing and choose the
+ * highest bid that is over the asking price
+ * @param {org.acme.vehicle.auction.CloseBidding} closeBidding - the closeBidding transaction
+ * @transaction
+ */
 async function closeBidding(closeBidding) {  // eslint-disable-line no-unused-vars
     const listing = closeBidding.listing;
-    if (listing.state !== 'OPEN') {         //Write something instead for_sale
-        throw new Error('Tender is not open for bidding');
+    if (listing.state !== 'OPEN') {
+        throw new Error('NOT OPEN');
     }
     // by default we mark the listing as RESERVE_NOT_MET
     listing.state = 'RESERVE_NOT_MET';
-    let lowestOffer = null;
-    let contractor = null;                       //Contractor
-    let department = null;                      //Department
+    let highestOffer = null;
+    let buyer = null;
+    let seller = null;
     if (listing.offers && listing.offers.length > 0) {
         // sort the bids by bidPrice
         listing.offers.sort(function(a, b) {
             return (a.bidPrice - b.bidPrice);
         });
-        lowestOffer = listing.offers[0];
-        if (lowestOffer.bidPrice <= listing.reservePrice) {
-            // mark the listing as CLOSED
+        highestOffer = listing.offers[0];
+        if (highestOffer.bidPrice <= listing.reservePrice) {
+            // mark the listing as SOLD
             listing.state = 'CLOSED';
-            contractor = lowestOffer.member;
-            department = listing.vehicle.owner;
-            // update the balance of the department
-            console.log('#### department balance before: ' + department.balance);
-            department.balance += lowestOffer.bidPrice;
-            console.log('#### department balance after: ' + department.balance);
-            //update the balance of the contractor
-            console.log('#### contractor balance before: ' + contractor.balance);
-            contractor.balance -= lowestOffer.bidPrice;
-            console.log('#### contractor balance after: ' + contractor.balance);
-            //transfer the vehicle to the contractor
-            listing.vehicle.owner = contractor;
+            buyer = highestOffer.member;
+            seller = listing.vehicle.owner;
+            // update the balance of the seller
+            console.log('#### seller balance before: ' + seller.balance);
+            seller.balance -= highestOffer.bidPrice;
+            console.log('#### seller balance after: ' + seller.balance);
+            // update the balance of the buyer
+            console.log('#### buyer balance before: ' + buyer.balance);
+            buyer.balance += highestOffer.bidPrice;
+            console.log('#### buyer balance after: ' + buyer.balance);
+            // transfer the vehicle to the buyer
+            listing.vehicle.owner = buyer;
             // clear the offers
             listing.offers = null;
         }
     }
 
-    if (lowestOffer) {
+    if (highestOffer) {
         // save the vehicle
         const vehicleRegistry = await getAssetRegistry('org.acme.vehicle.auction.Vehicle');
         await vehicleRegistry.update(listing.vehicle);
@@ -47,7 +69,7 @@ async function closeBidding(closeBidding) {  // eslint-disable-line no-unused-va
     if (listing.state === 'CLOSED') {
         // save the buyer
         const userRegistry = await getParticipantRegistry('org.acme.vehicle.auction.Member');
-        await userRegistry.updateAll([contractor, department]);
+        await userRegistry.updateAll([buyer, seller]);
     }
 }
 
@@ -59,91 +81,18 @@ async function closeBidding(closeBidding) {  // eslint-disable-line no-unused-va
 async function makeOffer(offer) {  // eslint-disable-line no-unused-vars
     let listing = offer.listing;
     if (listing.state !== 'OPEN') {
-        throw new Error('Tender is not open for bidding');
+        throw new Error('Listing is not FOR SALE');
     }
     if (!listing.offers) {
         listing.offers = [];
     }
+    if(listing.vehicle.owner == offer.member){
+        throw new Error("Owner can't bid");
+    }
     for (var i = 0; i < listing.offers.length; i++) {
-  if(listing.offers[i].member == offer.member){
-    throw new Error('Your bid has already been submitted');
-    }
-    }
-    listing.offers.push(offer);
-
-    // save the vehicle listing
-    const vehicleListingRegistry = await getAssetRegistry('org.acme.vehicle.auction.VehicleListing');
-    await vehicleListingRegistry.update(listing);
-async function closeBidding(closeBidding) {  // eslint-disable-line no-unused-vars
-    const listing = closeBidding.listing;
-    if (listing.state !== 'OPEN') {         //Write something instead for_sale
-        throw new Error('Tender is not open for bidding');
-    }
-    // by default we mark the listing as RESERVE_NOT_MET
-    listing.state = 'RESERVE_NOT_MET';
-    let lowestOffer = null;
-    let contractor = null;                       //Contractor
-    let department = null;                      //Department
-    if (listing.offers && listing.offers.length > 0) {
-        // sort the bids by bidPrice
-        listing.offers.sort(function(a, b) {
-            return (a.bidPrice - b.bidPrice);
-        });
-        lowestOffer = listing.offers[0];
-        if (lowestOffer.bidPrice <= listing.reservePrice) {
-            // mark the listing as CLOSED
-            listing.state = 'CLOSED';
-            contractor = lowestOffer.member;
-            department = listing.vehicle.owner;
-            // update the balance of the department
-            console.log('#### department balance before: ' + department.balance);
-            department.balance += lowestOffer.bidPrice;
-            console.log('#### department balance after: ' + department.balance);
-            //update the balance of the contractor
-            console.log('#### contractor balance before: ' + contractor.balance);
-            contractor.balance -= lowestOffer.bidPrice;
-            console.log('#### contractor balance after: ' + contractor.balance);
-            //transfer the vehicle to the contractor
-            listing.vehicle.owner = contractor;
-            // clear the offers
-            listing.offers = null;
+        if(listing.offers[i].member == offer.member){
+            throw new Error('Your bid has already been submitted');
         }
-    }
-
-    if (lowestOffer) {
-        // save the vehicle
-        const vehicleRegistry = await getAssetRegistry('org.acme.vehicle.auction.Vehicle');
-        await vehicleRegistry.update(listing.vehicle);
-    }
-
-    // save the vehicle listing
-    const vehicleListingRegistry = await getAssetRegistry('org.acme.vehicle.auction.VehicleListing');
-    await vehicleListingRegistry.update(listing);
-
-    if (listing.state === 'CLOSED') {
-        // save the buyer
-        const userRegistry = await getParticipantRegistry('org.acme.vehicle.auction.Member');
-        await userRegistry.updateAll([contractor, department]);
-    }
-}
-
-/**
- * Make an Offer for a VehicleListing
- * @param {org.acme.vehicle.auction.Offer} offer - the offer
- * @transaction
- */
-async function makeOffer(offer) {  // eslint-disable-line no-unused-vars
-    let listing = offer.listing;
-    if (listing.state !== 'OPEN') {
-        throw new Error('Tender is not open for bidding');
-    }
-    if (!listing.offers) {
-        listing.offers = [];
-    }
-    for (var i = 0; i < listing.offers.length; i++) {
-  if(listing.offers[i].member == offer.member){
-    throw new Error('Your bid has already been submitted');
-    }
     }
     listing.offers.push(offer);
 
